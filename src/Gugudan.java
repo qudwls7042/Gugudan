5b3dbc392f48430672e647953a2feeafc9829a43
qudwls7042 <64541839+qudwls7042@users.noreply.github.com>
2021-01-05 16:20:20 +0900

    구구단 6,7단 반복문 추가

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -2,19 +2,34 @@ import java.util.Scanner;
 
 	public class Gugudan {
 		public static void main(String[] args) {
-			System.out.printf("원하는 구구단 수를 입력하세요 : ");
 			
-			Scanner scanner = new Scanner(System.in);
-			int number = scanner.nextInt();
+			System.out.println("구구단 6단");
+			int i = 1;
+			while(i < 10) {
+				System.out.println(6 * i);
+				i = i + 1;
+		}
+			System.out.println("구구단 7단");
+			for(int j = 1; j < 10; j++) {
+				System.out.println(7 * j);
+			}
 			
-			System.out.println(number * 1);
-			System.out.println(number * 2);
-			System.out.println(number * 3);
-			System.out.println(number * 4);
-			System.out.println(number * 5);
-			System.out.println(number * 6);
-			System.out.println(number * 7);
-			System.out.println(number * 8);
-			System.out.println(number * 9);
+			
+//			System.out.printf("원하는 구구단 수를 입력하세요 : ");
+//			
+//			Scanner scanner = new Scanner(System.in);
+//			int number = scanner.nextInt();
+//			
+//			System.out.println(number * 1);
+//			System.out.println(number * 2);
+//			System.out.println(number * 3);
+//			System.out.println(number * 4);
+//			System.out.println(number * 5);
+//			System.out.println(number * 6);
+//			System.out.println(number * 7);
+//			System.out.println(number * 8);
+//			System.out.println(number * 9);
+			
+			
 		}
 }
